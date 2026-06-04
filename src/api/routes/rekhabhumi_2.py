287c33597c0bb731c1e45dc795889ad573515251
Moh Noor Al Azam, S.Kom., M.MT. <noor.azam@narotama.ac.id>
2026-06-04 14:38:31 +0700

    rekhabhumi_2.py untuk menangani permintaan /rekhabhumi/wilayah/<kode_wilayah>

--- src/api/routes/rekhabhumi_2.py
+++ src/api/routes/rekhabhumi_2.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from models.db import get_db, parse_kode
 from config import Config
 
-rekhabhumi_2_bp = Blueprint('rekhabhumi_2', __name__, url_prefix='/rekhabhumi')
+rekhabhumi_2_bp = Blueprint('rekhabhumi_2', __name__, url_prefix='/rekhabhumi/wilayah')
 
 @rekhabhumi_2_bp.before_request
 def log_request_info():
@@ -12,17 +12,35 @@ def log_request_info():
         print(f"Request URL: {request.url}", flush=True)
         print('=================================', flush=True)
 
-# @rekhabhumi_2_bp.route(...) diletakkan di bawah sini
-
-@rekhabhumi_2_bp.route('/wilayah', methods=['GET'], strict_slashes=False)
+@rekhabhumi_2_bp.route('/', methods=['GET'], strict_slashes=False)
 def error_wilayah():
-    return jsonify({'status': 'ERROR', 'message': f'Perhatikan format end-point. Lihat contoh di {Config.APP_URL}'}), 400
-@rekhabhumi_2_bp.route('/wilayah/<req_id:request_id>', methods=['GET'], strict_slashes=False)
-def get_wilayah(request_id):
-    
     return jsonify({
-        "status": "success",
-        "message": f"Berhasil diakses dari Blueprint! ID: {request_id}"
-    })
+        "status": "ERROR",
+        "invalid_input": request_id,
+        "message": "Format ID salah. Gunakan format xx, xx.xx, xx.xx.xx, atau xx.xx.xx.xxxx"
+    }), 400
 
+@rekhabhumi_2_bp.route('/<req_id:request_id>', methods=['GET'], strict_slashes=False)
+def get_wilayah(request_id):
+    query = f"SELECT kode, nama FROM wilayah WHERE kode LIKE '{request_id.strip()}' ORDER BY kode"  
+    db = get_db()
+    try:
+        with db.cursor() as cur:
+            cur.execute(query)
+            rows = cur.fetchall()
+        if len(rows) == 0:
+            return jsonify({'status': 'NOT_FOUND', 'total': 0, 'message': 'Data tidak ditemukan'}), 404
+        return jsonify({'status': 'SUCCESS', 'total': len(rows), 'data': rows})
+    except Exception as e:
+        return jsonify({'status': 'ERROR', 'message': str(e)}), 500
+    finally:
+        db.close()
+ 
+@rekhabhumi_2_bp.route('/<string:request_id>', methods=['GET'], strict_slashes=False)
+def get_wilayah_sink_home(request_id):
+    return jsonify({
+        "status": "ERROR",
+        "invalid_input": request_id,
+        "message": "Format ID salah. Gunakan format xx, xx.xx, xx.xx.xx, atau xx.xx.xx.xxxx"
+    }), 400
 
