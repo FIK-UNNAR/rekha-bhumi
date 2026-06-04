49807d9bc18aa6d2117e582ce26e958eb63ebb77
Moh Noor Al Azam, S.Kom., M.MT. <noor.azam@narotama.ac.id>
2026-06-04 13:42:40 +0700

    routes untuk rekhabhumi/list/<kodeWilayah>

--- src/api/routes/rekhabhumi.py
+++ src/api/routes/rekhabhumi.py
@@ -17,19 +17,23 @@ def log_request_info():
 @rekhabhumi_bp.route('/ver', methods=['GET'], strict_slashes=False)
 def versi():
     return jsonify({'status': 'OK', 'version': '1.0.0', 'app': Config.APP_NAME, 'data': 'Kemendagri 2024'})
+
 @rekhabhumi_bp.route('/list', methods=['GET'], strict_slashes=False)
 def error_list():
-    return jsonify({'status': 'ERROR', 'message': f'Perhatikan format end-point. Lihat contoh di {Config.APP_URL}'}), 400
+    #mengembalikan semua list provinsi di Indonesia
+    return jsonify({'status': 'SUCCESS', 'message': f'List semua provinsi di Indonesia'}), 200
 
 @rekhabhumi_bp.route('/list/<req_id:request_id>', methods=['GET'], strict_slashes=False)
 def get_list(request_id):
-    return jsonify({
-        "status": "success",
-        "message": f"Berhasil diakses dari Blueprint! ID: {request_id}"
-    })
-
-
-#@rekhabhumi_bp.route('/list/<format_kodewilayah:kodeWilayah>', methods=['GET'], strict_slashes=False)
-#def list_data(kodeWilayah):
-#    regex = r'\d+(?:\.\d+){0,2}(?:\.\d{4})?'
-#    return jsonify({'status': 'OK', 'message': f'Anda meminta data list {kodeWilayah}'})
+    match len(str(request_id.strip())):
+        case 2:
+            #mengembalikan semua list kota/kabupaten di provinsi di Indonesia
+            return jsonify({'status': 'SUCCESS', 'message': f'List kota/kabupaten di provinsi {request_id}'}), 200
+        case 5:
+            # Handle kabupaten
+            return jsonify({'status': 'SUCCESS', 'message': f'List Kecamatan di {request_id}'}), 200
+        case 8:
+            # Handle kecamatan
+            return jsonify({'status': 'SUCCESS', 'message': f'List Desa/Kelurahan di {request_id}'}), 200
+        case _:
+            return jsonify({'status': 'ERROR', 'message': f'Format request_id tidak valid. Perhatikan format end-point. Lihat contoh di {Config.APP_URL}'}), 400
