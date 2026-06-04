4cbffe19d27cedcf526bae569d0f45b0492f47b3
Moh Noor Al Azam, S.Kom., M.MT. <noor.azam@narotama.ac.id>
2026-06-04 10:03:51 +0700

    menambah check awal kode wilayah untuk memastikan formatnya benar, dan menambahkan endpoint root untuk dokumentasi API.

--- src/api/app.py
+++ src/api/app.py
@@ -1,22 +1,29 @@
 from flask import Flask, request, render_template
+from werkzeug.routing import BaseConverter
 from flask_cors import CORS
 from config import Config
 import subprocess
 from pathlib import Path
 import shutil
 from routes.rekhabhumi import rekhabhumi_bp
-#from routes.wilayah import wilayah_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
 if Config.FLASK_ENV not in ['development', 'production']:
     print(f"FATAL ERROR: Unrecognized FLASK_ENV value '{Config.FLASK_ENV}'.", flush=True)
     sys.exit(1)
-if Config.FLASK_ENV == 'development':
+
+if Config.FLASK_ENV == 'development': #jika development, jangan cache file statis.
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.register_blueprint(rekhabhumi_bp, url_prefix='/rekhabhumi')
 
+class RequestIdConverter(BaseConverter):
+    regex = r'^(?:\d{2}|\d{2}\.\d{2}|\d{2}\.\d{2}\.\d{2}|\d{2}\.\d{2}\.\d{2}\.\d{4})$'
+
+app.url_map.converters['req_id'] = RequestIdConverter
+app.register_blueprint(rekhabhumi_bp, url_prefix='/rekhabhumi') #fungsi-fungsi API ada di blueprint -lokasi file: routes/rekhabhumi.py
+
+#root endpoint untuk informasi dan dokumentasi API -statif file di templates/index.html
 @app.route('/rekhabhumi/', methods=['GET'], strict_slashes=False)
 def root():
     data_endpoint = [
@@ -25,8 +32,6 @@ def root():
         {"path": "/rekhabhumi/provinsi/", "deskripsi": "Mendapatkan daftar provinsi"},
         {"path": "/rekhabhumi/kabupaten/", "deskripsi": "Mendapatkan daftar kabupaten berdasarkan provinsi"}
     ]
-    
-    # 2. Render template dan lempar datanya (Ini adalah konsep MVC!)
     return render_template(
         'index.html', 
         title="Dokumentasi API", 
