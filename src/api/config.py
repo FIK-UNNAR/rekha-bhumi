e26c3336d5a968cb1d9d236674ca0e35d290a3c6
Moh Noor Al Azam, S.Kom., M.MT. <noor.azam@narotama.ac.id>
2026-06-04 10:03:04 +0700

    update example ini dan config menyesuaikan kebutuhan

--- src/api/config.py
+++ src/api/config.py
@@ -1,8 +1,9 @@
 import os
 
 class Config:
-    APP_NAME = os.getenv('APP_NAME', 'Rekha Bhumi Nusantara')
-    FLASK_ENV = os.getenv('FLASK_ENV', 'development')
+    APP_NAME    = os.getenv('APP_NAME', 'Rekha Bhumi Nusantara')
+    FLASK_ENV   = os.getenv('FLASK_ENV', 'development')
+    APP_URL     = os.getenv('APP_URL', 'http://api.testing.id/rekhabhumi')
     DB_HOST     = os.getenv('DB_HOST', '127.0.0.1')
     DB_PORT     = int(os.getenv('DB_PORT', 3306))   # ← wajib int, bukan string
     DB_USER     = os.getenv('DB_USER', 'rekha_bhumi')
